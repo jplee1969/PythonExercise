52341b7640a3c8e4039930a89275fd51179fe785
jplee1969 <jplee@163.com>
2017-07-07 08:34:47 +0800

    练习

---
+++ PyCallenge-2.py
@@ -0,0 +1,8 @@
+s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+r=""
+for i in s:
+    d = ord(i)
+    if i.islower():
+        d = (d - ord('a') + 2) % 26 + ord('a')
+    r += chr(d)
+print(r)
---
+++ ex.py
@@ -0,0 +1,22 @@
+# n = int(input())
+# sum = 0;
+# for i in range(1,n):
+#     if i % 3 == 0 or i % 5 == 0:
+#         sum += i
+# print(sum)
+
+import math
+def isprime(x):
+    if x < 2:
+        return False
+    for i in range(2,int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+    return True
+
+n = int(input())
+sum = 0
+for i in range(n):
+    if isprime(i):
+        sum += i
+print(sum)
